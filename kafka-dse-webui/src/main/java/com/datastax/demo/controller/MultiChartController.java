36634c3387dec3ee67aa69133a7cc37804de04de
Alexandre Dutra <alexdut@gmail.com>
2019-02-18 14:21:47 +0100

    Fix typo

--- kafka-dse-webui/src/main/java/com/datastax/demo/controller/MultiChartController.java
+++ kafka-dse-webui/src/main/java/com/datastax/demo/controller/MultiChartController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * Home page
  *
- * <p>List all ticker meta data from table 'ticker_info'. This tabke has been loaded by producer at
+ * <p>List all ticker meta data from table 'ticker_info'. This table has been loaded by producer at
  * startup with lines of the CSV.
  */
 @Controller
